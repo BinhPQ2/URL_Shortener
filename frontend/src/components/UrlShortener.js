ba5e83d4cd17226a0f88575c45c2341558900ccb
BinhPQ <binhphamvb@gmail.com>
2024-09-25 22:51:29 +0700

    add boostrap

--- frontend/src/components/UrlShortener.js
+++ frontend/src/components/UrlShortener.js
@@ -11,22 +11,30 @@ const UrlShortener = () => {
         setShortUrl(generatedShortUrl);
     };
 
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(shortUrl).then(() => {
+            alert('Short URL copied to clipboard!');
+        });
+    };
+    
     return (
-        <div className="url-shortener">
-            <form onSubmit={handleSubmit}>
+        <div className="url-shortener container mt-5">
+            <form className="form-group" onSubmit={handleSubmit}>
                 <label htmlFor="url-input">Enter Long URL:</label>
                 <input
                     type="url"
                     id="url-input"
+                    className="form-control"
                     value={longUrl}
                     onChange={(e) => setLongUrl(e.target.value)}
                     required
                 />
-                <button type="submit">Shorten URL</button>
+                <button type="submit" className="btn btn-primary mt-3">Shorten URL</button>
             </form>
             {shortUrl && (
-                <div className="result">
+                <div className="result mt-4">
                     <p>Shortened URL: <a href={shortUrl} target="_blank" rel="noopener noreferrer">{shortUrl}</a></p>
+                    <button className="btn btn-secondary" onClick={copyToClipboard}>Copy to Clipboard</button>
                 </div>
             )}
         </div>
@@ -34,3 +42,6 @@ const UrlShortener = () => {
 };
 
 export default UrlShortener;
+
+
+
